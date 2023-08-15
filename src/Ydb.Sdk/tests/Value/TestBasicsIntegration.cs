b62d69b4a85d30741a36ae83aac2663b395ded80
robot <robot@umbrella>
2023-08-15 13:17:27 +0300

    Add tests for optional values

--- src/Ydb.Sdk/tests/Value/TestBasicsIntegration.cs
+++ src/Ydb.Sdk/tests/Value/TestBasicsIntegration.cs
@@ -163,6 +163,111 @@ namespace Ydb.Sdk.Value.Tests
 
 
         [Fact]
+        public async Task OptionalPrimitiveTypes()
+        {
+            var valueBool = true;
+            var resultBool = await SelectPassed(YdbValue.MakeOptionalBool(valueBool));
+            Assert.Equal(resultBool.GetOptionalBool(), valueBool);
+
+            var valueInt8 = (sbyte)-1;
+            var resultInt8 = await SelectPassed(YdbValue.MakeOptionalInt8(valueInt8));
+            Assert.Equal(resultInt8.GetOptionalInt8(), valueInt8);
+
+            var valueUint8 = (byte)200;
+            var resultUint8 = await SelectPassed(YdbValue.MakeOptionalUint8(valueUint8));
+            Assert.Equal(resultUint8.GetOptionalUint8(), valueUint8);
+
+            var valueInt16 = (short)-200;
+            var resultInt16 = await SelectPassed(YdbValue.MakeOptionalInt16(valueInt16));
+            Assert.Equal(resultInt16.GetOptionalInt16(), valueInt16);
+
+            var valueUint16 = (ushort)40_000;
+            var resultUint16 = await SelectPassed(YdbValue.MakeOptionalUint16(valueUint16));
+            Assert.Equal(resultUint16.GetOptionalUint16(), valueUint16);
+
+            var valueInt32 = -40000;
+            var resultInt32 = await SelectPassed(YdbValue.MakeOptionalInt32(valueInt32));
+            Assert.Equal(resultInt32.GetOptionalInt32(), valueInt32);
+
+            var valueUint32 = 4_000_000_000;
+            var resultUint32 = await SelectPassed(YdbValue.MakeOptionalUint32(valueUint32));
+            Assert.Equal(resultUint32.GetOptionalUint32(), valueUint32);
+
+            var valueInt64 = -4_000_000_000;
+            var resultInt64 = await SelectPassed(YdbValue.MakeOptionalInt64(valueInt64));
+            Assert.Equal(resultInt64.GetOptionalInt64(), valueInt64);
+
+            var valueUint64 = 10_000_000_000ul;
+            var resultUint64 = await SelectPassed(YdbValue.MakeOptionalUint64(valueUint64));
+            Assert.Equal(resultUint64.GetOptionalUint64(), valueUint64);
+
+            var valueFloat = -1.7f;
+            var resultFloat = await SelectPassed(YdbValue.MakeOptionalFloat(valueFloat));
+            Assert.Equal(resultFloat.GetOptionalFloat(), valueFloat);
+
+            var valueDouble = 123.45;
+            var resultDouble = await SelectPassed(YdbValue.MakeOptionalDouble(valueDouble));
+            Assert.Equal(resultDouble.GetOptionalDouble(), valueDouble);
+
+            var valueDate = new DateTime(2021, 08, 21);
+            var resultDate = await SelectPassed(YdbValue.MakeOptionalDate(valueDate));
+            Assert.Equal(resultDate.GetOptionalDate(), valueDate);
+
+            var valueDatetime = new DateTime(2021, 08, 21, 23, 30, 47);
+            var resultDatetime = await SelectPassed(YdbValue.MakeOptionalDatetime(valueDatetime));
+            Assert.Equal(resultDatetime.GetOptionalDatetime(), valueDatetime);
+
+            var valueTimestamp = new DateTime(2021, 08, 21, 23, 30, 47, 581);
+            var resultTimestamp = await SelectPassed(YdbValue.MakeOptionalTimestamp(valueTimestamp));
+            Assert.Equal(resultTimestamp.GetOptionalTimestamp(), valueTimestamp);
+
+            var valueInterval = -new TimeSpan(3, 7, 40, 27, 729);
+            var resultInterval = await SelectPassed(YdbValue.MakeOptionalInterval(valueInterval));
+            Assert.Equal(resultInterval.GetOptionalInterval(), valueInterval);
+
+            var valueString = Encoding.ASCII.GetBytes("test str");
+            var resultString = await SelectPassed(YdbValue.MakeOptionalString(valueString));
+            Assert.Equal(resultString.GetOptionalString(), valueString);
+
+            var valueUtf8 = "unicode str";
+            var resultUtf8 = await SelectPassed(YdbValue.MakeOptionalUtf8(valueUtf8));
+            Assert.Equal(resultUtf8.GetOptionalUtf8(), valueUtf8);
+
+            var valueYson = Encoding.ASCII.GetBytes("{type=\"yson\"}");
+            var resultYson = await SelectPassed(YdbValue.MakeOptionalYson(valueYson));
+            Assert.Equal(resultYson.GetOptionalYson(), valueYson);
+
+            var valueJson = "{\"type\": \"json\"}";
+            var resultJson = await SelectPassed(YdbValue.MakeOptionalJson(valueJson));
+            Assert.Equal(resultJson.GetOptionalJson(), valueJson);
+
+            var valueJsonDocument = "{\"type\":\"jsondoc\"}";
+            var resultJsonDocument = await SelectPassed(YdbValue.MakeOptionalJsonDocument(valueJsonDocument));
+            Assert.Equal(resultJsonDocument.GetOptionalJsonDocument(), valueJsonDocument);
+
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalBool(null))).GetOptionalBool());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalInt8(null))).GetOptionalInt8());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalUint8(null))).GetOptionalUint8());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalInt16(null))).GetOptionalInt16());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalUint16(null))).GetOptionalUint16());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalInt32(null))).GetOptionalInt32());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalUint32(null))).GetOptionalUint32());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalInt64(null))).GetOptionalInt64());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalUint64(null))).GetOptionalUint64());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalFloat(null))).GetOptionalFloat());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalDouble(null))).GetOptionalDouble());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalDate(null))).GetOptionalDate());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalDatetime(null))).GetOptionalDatetime());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalTimestamp(null))).GetOptionalTimestamp());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalInterval(null))).GetOptionalInterval());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalString(null))).GetOptionalString());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalUtf8(null))).GetOptionalUtf8());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalYson(null))).GetOptionalYson());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalJson(null))).GetOptionalJson());
+            Assert.Null((await SelectPassed(YdbValue.MakeOptionalJsonDocument(null))).GetOptionalJsonDocument());
+        }
+
+        [Fact]
         public async Task ListType()
         {
             var value = new[] { YdbValue.MakeUtf8("one"), YdbValue.MakeUtf8("two") };
@@ -215,10 +320,10 @@ namespace Ydb.Sdk.Value.Tests
 
             var foo1 = result.GetStruct()["foo1"].GetStruct();
             var foo2 = result.GetStruct()["foo2"].GetStruct();
-            
+
             Assert.Empty(foo1);
             Assert.Equal(2, foo2.Count);
-            
+
             Assert.Equal(10, foo2["bar1"].GetInt64());
             Assert.Equal("ten", foo2["bar2"].GetUtf8());
         }
