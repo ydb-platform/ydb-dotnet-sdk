7d390cd2299eb27360737cf32e9b7b34ed5a4048
XmasApple <y4blokof@yandex-team.ru>
2023-10-12 16:25:35 +0300

    CopyTableMethod

--- src/Ydb.Sdk/tests/Table/TestDescribeTable.cs
+++ src/Ydb.Sdk/tests/Table/TestDescribeTable.cs
@@ -2,7 +2,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
 using Ydb.Sdk.Services.Table;
-using Ydb.Sdk.Tests.Auth;
 
 namespace Ydb.Sdk.Tests.Table;
 
@@ -10,8 +9,6 @@ namespace Ydb.Sdk.Tests.Table;
 public class TestDescribeTable
 {
     private readonly ILoggerFactory? _loggerFactory;
-    private readonly ILogger _logger;
-
     private readonly DriverConfig _driverConfig = new(
         endpoint: "grpc://localhost:2136",
         database: "/local"
@@ -20,17 +17,14 @@ public class TestDescribeTable
     public TestDescribeTable()
     {
         _loggerFactory = Utils.GetLoggerFactory() ?? NullLoggerFactory.Instance;
-        _logger = _loggerFactory.CreateLogger<TestStaticAuth>();
     }
 
 
     [Fact]
     public async Task DescribeNotExisting()
     {
-        _logger.LogInformation("Initializing driver and client");
         await using var driver = await Driver.CreateInitialized(_driverConfig, _loggerFactory);
         using var tableClient = new TableClient(driver);
-        _logger.LogInformation("Driver and client was initialized");
 
         var response = await tableClient.DescribeTable("/local/not_exists");
         Assert.Equal(StatusCode.SchemeError, response.Status.StatusCode);
@@ -40,10 +34,8 @@ public class TestDescribeTable
     [Fact]
     public async Task CreateAndDescribe()
     {
-        _logger.LogInformation("Initializing driver and client");
         await using var driver = await Driver.CreateInitialized(_driverConfig, _loggerFactory);
         using var tableClient = new TableClient(driver);
-        _logger.LogInformation("Driver and client was initialized");
 
         var tablePath = $"t{Guid.NewGuid():n}";
         const string columnName = "myColumnName";
