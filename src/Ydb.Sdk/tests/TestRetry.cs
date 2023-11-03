e6f62e27ffefe4d07814a070bb1416ad707cc58d
XmasApple <y4blokof@yandex-team.ru>
2023-11-03 10:18:00 +0300

    add test for wrong status codes in retry

--- src/Ydb.Sdk/tests/TestRetry.cs
+++ src/Ydb.Sdk/tests/TestRetry.cs
@@ -1,27 +1,37 @@
+using Google.Protobuf.Collections;
 using Xunit;
-using Xunit.Abstractions;
+using Ydb.Issue;
 
 namespace Ydb.Sdk.Tests;
 
 [Trait("Category", "Unit")]
 public class TestRetry
 {
-    // ReSharper disable once NotAccessedField.Local
-    private readonly ITestOutputHelper _output;
-
-    public TestRetry(ITestOutputHelper output)
-    {
-        _output = output;
-    }
+    private const StatusCode WrongStatusCode = (StatusCode)123456; // there is no status code with this value 
 
     [Fact]
     public void GetRetryRuleOutOfRange()
     {
         var retrySettings = new RetrySettings();
-        foreach (var statusCode in (StatusCode[]) Enum.GetValues(typeof(StatusCode)))
+        foreach (var statusCode in (StatusCode[])Enum.GetValues(typeof(StatusCode)))
         {
             var exception = Record.Exception(() => retrySettings.GetRetryRule(statusCode));
             Assert.Null(exception);
         }
+
+        Assert.DoesNotContain(WrongStatusCode, (StatusCode[])Enum.GetValues(typeof(StatusCode)));
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => { retrySettings.GetRetryRule(WrongStatusCode); });
+    }
+
+    [Fact]
+    public void ConvertWrongGrpcStatusCode()
+    {
+        Assert.DoesNotContain(WrongStatusCode, (StatusCode[])Enum.GetValues(typeof(StatusCode)));
+
+        var status = Status.FromProto(
+            statusCode: (StatusIds.Types.StatusCode)WrongStatusCode,
+            new RepeatedField<IssueMessage>());
+        Assert.Equal(StatusCode.Unspecified, status.StatusCode);
     }
 }
