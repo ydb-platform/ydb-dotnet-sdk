4054e894b2cb941b864172072e3a875430c6cef8
Timofey Koolin <rekby@yandex-team.com>
2023-12-15 12:17:27 +0300

    using session

--- slo/src/Client.cs
+++ slo/src/Client.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Ydb.Sdk;
 using Ydb.Sdk.Services.Table;
+using Ydb.Sdk.Services.Sessions;
 
 namespace slo;
 
