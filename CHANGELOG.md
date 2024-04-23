b26a7759a533eafdc3a0df063a2992dd732cff9d
robot <robot@umbrella>
2024-04-23 17:32:21 +0000

    Release v0.2.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+## v0.2.3
 - Add rollback transaction API
 
 ## v0.2.2
--- src/Ydb.Sdk/src/Version.cs
+++ src/Ydb.Sdk/src/Version.cs
@@ -7,5 +7,5 @@ public static class Version
 {
     public const uint Major = 0;
     public const uint Minor = 2;
-    public const uint Patch = 2;
+    public const uint Patch = 3;
 }
