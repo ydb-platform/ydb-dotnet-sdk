9bdb065027d6daea8e7d0fa7ba2b1e85a2c8b143
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2024-05-06 17:02:41 +0300

    Impl FixedGrpcChannelTransport.cs

---
+++ src/Ydb.Sdk/src/Transport/FixedGrpcChannelTransport.cs
@@ -0,0 +1,41 @@
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Logging;
+
+namespace Ydb.Sdk.Transport;
+
+internal class FixedGrpcChannelTransport : GrpcTransport
+{
+    private readonly GrpcChannel _channel;
+
+    public FixedGrpcChannelTransport(
+        DriverConfig driverConfig,
+        ILogger logger
+    ) : base(driverConfig, logger)
+    {
+        _channel = ChannelsCache.CreateChannel(Config.Endpoint, Config);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _channel.Dispose();
+        }
+    }
+
+    protected override (string, GrpcChannel) GetChannel()
+    {
+        return (Config.Endpoint, _channel);
+    }
+
+    protected override void OnRpcError(string endpoint, RpcException e)
+    {
+        var status = e.Status;
+        if (e.Status.StatusCode != Grpc.Core.StatusCode.OK)
+        {
+            Logger.LogWarning("gRPC error {StatusCode}[{Detail}] on fixed channel {Endpoint}",
+                status.StatusCode, status.Detail, endpoint);
+        }
+    }
+}
