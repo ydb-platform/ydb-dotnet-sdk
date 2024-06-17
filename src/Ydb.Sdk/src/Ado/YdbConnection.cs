9d1884c70727a35d06a91bacc46f48d20f0760d8
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2024-06-17 16:43:51 +0700

    feat: impl DbTransaction, first step in impl YdbConnection

---
+++ src/Ydb.Sdk/src/Ado/YdbConnection.cs
@@ -0,0 +1,62 @@
+using System.Data;
+using System.Data.Common;
+
+namespace Ydb.Sdk.Ado;
+
+public sealed class YdbConnection : DbConnection
+{
+    private readonly YdbContext _ydbContext;
+
+    internal IYdbConnectionState YdbConnectionState { get; private set; }
+
+    internal void NextOutTransactionState()
+    {
+        YdbConnectionState = new OutTransactionState(); // TODO
+    }
+
+    internal YdbConnection(YdbContext ydbContext)
+    {
+        _ydbContext = ydbContext;
+        YdbConnectionState = new OutTransactionState();
+    }
+
+    protected override YdbTransaction BeginDbTransaction(IsolationLevel isolationLevel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ChangeDatabase(string databaseName)
+    {
+    }
+
+    public override void Close()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Open()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string ConnectionString
+    {
+        get => _ydbContext.ConnectionString;
+        set
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            throw new YdbAdoException("TODO");
+        }
+    }
+
+    public override string Database => _ydbContext.Database;
+
+    public override ConnectionState State => ConnectionState.Open; // TODO
+    public override string DataSource => string.Empty; // TODO
+    public override string ServerVersion => string.Empty; // TODO
+
+    protected override YdbCommand CreateDbCommand()
+    {
+        return new YdbCommand(this);
+    }
+}
---
+++ src/Ydb.Sdk/src/Ado/YdbConnectionState.cs
@@ -0,0 +1,105 @@
+using Ydb.Sdk.Services.Query;
+using Ydb.Sdk.Value;
+using Session = Ydb.Sdk.Services.Query.Pool.Session;
+
+namespace Ydb.Sdk.Ado;
+
+internal interface IYdbConnectionState
+{
+    Task Commit();
+
+    Task Rollback();
+
+    IReadOnlyList<ExecuteQueryPart> ExecuteQuery(string query, Dictionary<string, YdbValue>? parameters);
+
+    bool IsClosed => false;
+}
+
+internal sealed class OutTransactionState : IYdbConnectionState
+{
+    public Task Commit()
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task Rollback()
+    {
+        return Task.CompletedTask;
+    }
+
+    public IReadOnlyList<ExecuteQueryPart> ExecuteQuery(string query, Dictionary<string, YdbValue>? parameters)
+    {
+        throw new NotImplementedException(); // TODO QueryClient
+    }
+}
+
+internal sealed class InTransactionState : IYdbConnectionState
+{
+    private readonly Session _session;
+
+    internal string? TxId { get; set; }
+
+    internal InTransactionState(Session session)
+    {
+        _session = session;
+    }
+
+    public async Task Commit()
+    {
+        await TransactionFinish(txId => _session.RollbackTransaction(txId));
+    }
+
+    public async Task Rollback()
+    {
+        await TransactionFinish(txId => _session.RollbackTransaction(txId));
+    }
+
+    private async Task TransactionFinish(Func<string, Task<Status>> finishMethod)
+    {
+        if (TxId == null)
+        {
+            return;
+        }
+
+        try
+        {
+            var status = await finishMethod(TxId); // TODO Maybe retry on transport status
+
+            if (status.IsNotSuccess)
+            {
+                throw new YdbAdoException(status);
+            }
+        }
+        finally
+        {
+            _session.Release();
+        }
+    }
+
+    public IReadOnlyList<ExecuteQueryPart> ExecuteQuery(string query, Dictionary<string, YdbValue>? parameters)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal sealed class ClosedState : IYdbConnectionState
+{
+    private const string ClosedStateMessage = "Connection closed";
+
+    public Task Commit()
+    {
+        throw new YdbAdoException(ClosedStateMessage);
+    }
+
+    public Task Rollback()
+    {
+        throw new YdbAdoException(ClosedStateMessage);
+    }
+
+    public IReadOnlyList<ExecuteQueryPart> ExecuteQuery(string query, Dictionary<string, YdbValue>? parameters)
+    {
+        throw new YdbAdoException(ClosedStateMessage);
+    }
+
+    public bool IsClosed => false;
+}
