9e7e036a3c42c4c00c5171c04c475687d1470dc4
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2024-08-13 21:42:08 +0300

    fix query

--- slo/src/Internal/SloContext.cs
+++ slo/src/Internal/SloContext.cs
@@ -37,7 +37,7 @@ public abstract class SloContext<T> where T : IDisposable
                          AUTO_PARTITIONING_BY_LOAD = ENABLED,
                          AUTO_PARTITIONING_PARTITION_SIZE_MB = ${config.PartitionSize},
                          AUTO_PARTITIONING_MIN_PARTITIONS_COUNT = ${config.MinPartitionsCount},
-                         AUTO_PARTITIONING_MAX_PARTITIONS_COUNT = ${config.MaxPartitionsCount},
+                         AUTO_PARTITIONING_MAX_PARTITIONS_COUNT = ${config.MaxPartitionsCount}
                      );
                      """, config.WriteTimeout);
 
