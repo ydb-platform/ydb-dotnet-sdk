3d05660201b5071bdaf5ab8259af37f21f86f623
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2024-08-16 12:17:45 +0300

    added timout on tests

--- src/Ydb.Sdk/tests/Auth/StaticAuthTests.cs
+++ src/Ydb.Sdk/tests/Auth/StaticAuthTests.cs
@@ -88,26 +88,26 @@ public class StaticAuthTests : IDisposable
     }
 
 
-    [Fact]
+    [Fact(Timeout = 5_000)]
     public async Task GoodAuth()
     {
         await CheckAuth("test_password", "test_password");
     }
 
-    [Fact]
+    [Fact(Timeout = 5_000)]
     public async Task NoPasswordAuth()
     {
         await CheckAuth(null, null);
     }
 
-    [Fact]
+    [Fact(Timeout = 5_000)]
     public async Task WrongPassword()
     {
         await Assert.ThrowsAsync<InvalidCredentialsException>(
             async () => await CheckAuth("good_password", "wrong_password", maxRetries: 1));
     }
 
-    [Fact]
+    [Fact(Timeout = 5_000)]
     public async Task NotExistAuth()
     {
         var driverConfig = new DriverConfig(
