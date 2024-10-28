d34c568cc22f28a6a03145b43c5b1347a536a47e
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2024-10-28 16:51:23 +0300

    fix linter

--- src/Ydb.Sdk/src/Services/Topic/Reader/ReaderBuilder.cs
+++ src/Ydb.Sdk/src/Services/Topic/Reader/ReaderBuilder.cs
@@ -2,14 +2,14 @@ namespace Ydb.Sdk.Services.Topic.Reader;
 
 public class ReaderBuilder<TValue>
 {
-    private readonly ReaderConfig _config;
-    private readonly Driver _driver;
-
-    public ReaderBuilder(Driver driver, ReaderConfig config)
-    {
-        _driver = driver;
-        _config = config;
-    }
+    // private readonly ReaderConfig _config;
+    // private readonly Driver _driver;
+    //
+    // public ReaderBuilder(Driver driver, ReaderConfig config)
+    // {
+    //     _driver = driver;
+    //     _config = config;
+    // }
 
     public IDeserializer<TValue>? Deserializer { get; set; }
 
