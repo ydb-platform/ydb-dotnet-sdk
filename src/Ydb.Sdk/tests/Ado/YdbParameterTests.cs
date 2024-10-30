d4a9f4fccb9c7a001ccb5becf1e7a6bdf06bd651
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2024-10-30 16:09:28 +0300

    fix tests

--- src/Ydb.Sdk/tests/Ado/YdbParameterTests.cs
+++ src/Ydb.Sdk/tests/Ado/YdbParameterTests.cs
@@ -80,7 +80,6 @@ public class YdbParameterTests
     [Theory]
     [InlineData(DbType.VarNumeric, "VarNumeric")]
     [InlineData(DbType.Xml, "Xml")]
-    [InlineData(DbType.Guid, "Guid")]
     [InlineData(DbType.Time, "Time")]
     public void YdbValue_WhenNoSupportedDbType_ThrowException(DbType dbType, string name)
     {
