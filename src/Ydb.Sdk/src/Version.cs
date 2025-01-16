40a3b478c6c87dc553ee5e4eeeab3d09f1f9453c
robot <robot@umbrella>
2025-01-16 04:41:45 +0000

    Release v0.10.0

--- src/Ydb.Sdk/src/Version.cs
+++ src/Ydb.Sdk/src/Version.cs
@@ -3,6 +3,6 @@ namespace Ydb.Sdk;
 public static class Version
 {
     public const uint Major = 0;
-    public const uint Minor = 9;
-    public const uint Patch = 4;
+    public const uint Minor = 10;
+    public const uint Patch = 0;
 }
