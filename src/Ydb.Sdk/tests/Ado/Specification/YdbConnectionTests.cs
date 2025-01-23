611ed9d98c5a6b7917f0ad9904383b24164c3a07
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-01-23 12:49:32 +0300

    skip OpenAsync

--- src/Ydb.Sdk/tests/Ado/Specification/YdbConnectionTests.cs
+++ src/Ydb.Sdk/tests/Ado/Specification/YdbConnectionTests.cs
@@ -40,4 +40,12 @@ public class YdbConnectionTests : ConnectionTestBase<YdbFactoryFixture>
     {
         base.ServerVersion_returns_value();
     }
+
+#pragma warning disable xUnit1004
+    [Fact(Skip = "TODO Supported cancel OpenAsync.")]
+#pragma warning restore xUnit1004
+    public override Task OpenAsync_is_canceled()
+    {
+        return base.OpenAsync_is_canceled();
+    }
 }
