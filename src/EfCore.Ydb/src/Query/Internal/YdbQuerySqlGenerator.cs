2299166b4da78eba4f64af55efe7261f7f40d879
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-03-24 11:43:35 +0300

    fix linter

--- src/EfCore.Ydb/src/Query/Internal/YdbQuerySqlGenerator.cs
+++ src/EfCore.Ydb/src/Query/Internal/YdbQuerySqlGenerator.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore.Query;
