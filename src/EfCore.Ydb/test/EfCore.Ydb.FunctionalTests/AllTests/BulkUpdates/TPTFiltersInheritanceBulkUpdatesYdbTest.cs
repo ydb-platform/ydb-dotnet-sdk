d6dd423316df061c93cf1e841b2aec5c010889e6
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-03-30 15:51:43 +0300

    fix linter

--- src/EfCore.Ydb/test/EfCore.Ydb.FunctionalTests/AllTests/BulkUpdates/TPTFiltersInheritanceBulkUpdatesYdbTest.cs
+++ src/EfCore.Ydb/test/EfCore.Ydb.FunctionalTests/AllTests/BulkUpdates/TPTFiltersInheritanceBulkUpdatesYdbTest.cs
@@ -12,45 +12,45 @@ internal class TPTFiltersInheritanceBulkUpdatesSqlServerTest(
     ITestOutputHelper testOutputHelper
 ) : TPTFiltersInheritanceBulkUpdatesTestBase<TPTFiltersInheritanceBulkUpdatesYdbFixture>(fixture, testOutputHelper)
 {
-    [ConditionalTheory(Skip = "https://github.com/ydb-platform/ydb/issues/15177")]
-    [MemberData(nameof(IsAsyncData))]
-    public override Task Delete_where_using_hierarchy(bool async)
-        => Task.CompletedTask;
+    // [ConditionalTheory(Skip = "https://github.com/ydb-platform/ydb/issues/15177")]
+    // [MemberData(nameof(IsAsyncData))]
+    // public override Task Delete_where_using_hierarchy(bool async)
+    //     => Task.CompletedTask;
 
-    [ConditionalTheory(Skip = "https://github.com/ydb-platform/ydb/issues/15177")]
-    [MemberData(nameof(IsAsyncData))]
-    public override Task Delete_where_using_hierarchy_derived(bool async)
-        => Task.CompletedTask;
+    // [ConditionalTheory(Skip = "https://github.com/ydb-platform/ydb/issues/15177")]
+    // [MemberData(nameof(IsAsyncData))]
+    // public override Task Delete_where_using_hierarchy_derived(bool async)
+    //     => Task.CompletedTask;
 
-    [ConditionalTheory(Skip = "need fix")]
-    [MemberData(nameof(IsAsyncData))]
-    public override Task Update_base_type(bool async)
-        => Task.CompletedTask;
+    // [ConditionalTheory(Skip = "need fix")]
+    // [MemberData(nameof(IsAsyncData))]
+    // public override Task Update_base_type(bool async)
+    //     => Task.CompletedTask;
+    //
+    // [ConditionalTheory(Skip = "need fix")]
+    // [MemberData(nameof(IsAsyncData))]
+    // public override Task Update_base_type_with_OfType(bool async)
+    //     => Task.CompletedTask;
 
-    [ConditionalTheory(Skip = "need fix")]
-    [MemberData(nameof(IsAsyncData))]
-    public override Task Update_base_type_with_OfType(bool async)
-        => Task.CompletedTask;
+    // [ConditionalTheory(Skip = "need fix")]
+    // [MemberData(nameof(IsAsyncData))]
+    // public override Task Update_base_property_on_derived_type(bool async)
+    //     => Task.CompletedTask;
 
-    [ConditionalTheory(Skip = "need fix")]
-    [MemberData(nameof(IsAsyncData))]
-    public override Task Update_base_property_on_derived_type(bool async)
-        => Task.CompletedTask;
-
-    [ConditionalTheory(Skip = "need fix")]
-    [MemberData(nameof(IsAsyncData))]
-    public override Task Update_derived_property_on_derived_type(bool async)
-        => Task.CompletedTask;
-
-    [ConditionalTheory(Skip = "https://github.com/ydb-platform/ydb/issues/15177")]
-    [MemberData(nameof(IsAsyncData))]
-    public override Task Update_where_using_hierarchy(bool async)
-        => Task.CompletedTask;
-
-    [ConditionalTheory(Skip = "https://github.com/ydb-platform/ydb/issues/15177")]
-    [MemberData(nameof(IsAsyncData))]
-    public override Task Update_where_using_hierarchy_derived(bool async)
-        => Task.CompletedTask;
+    // [ConditionalTheory(Skip = "need fix")]
+    // [MemberData(nameof(IsAsyncData))]
+    // public override Task Update_derived_property_on_derived_type(bool async)
+    //     => Task.CompletedTask;
+    //
+    // [ConditionalTheory(Skip = "https://github.com/ydb-platform/ydb/issues/15177")]
+    // [MemberData(nameof(IsAsyncData))]
+    // public override Task Update_where_using_hierarchy(bool async)
+    //     => Task.CompletedTask;
+    //
+    // [ConditionalTheory(Skip = "https://github.com/ydb-platform/ydb/issues/15177")]
+    // [MemberData(nameof(IsAsyncData))]
+    // public override Task Update_where_using_hierarchy_derived(bool async)
+    //     => Task.CompletedTask;
 
     protected override void ClearLog()
         => Fixture.TestSqlLoggerFactory.Clear();
