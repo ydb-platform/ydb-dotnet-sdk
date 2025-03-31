2aac56704d37c49c1ec2d53333f312f44d009d24
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-03-31 17:25:57 +0300

    Example EF & YC

---
+++ examples/src/EF_YC/Program.cs
@@ -0,0 +1,48 @@
+﻿using CommandLine;
+using EF_YC;
+using EfCore.Ydb.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ydb.Sdk.Yc;
+
+await Parser.Default.ParseArguments<CmdOptions>(args).WithParsedAsync(async cmd =>
+{
+    var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Information));
+    var saProvider = new ServiceAccountProvider(saFilePath: cmd.SaFilePath, loggerFactory: loggerFactory);
+
+    var options = new DbContextOptionsBuilder<AppDbContext>()
+        .UseYdb(cmd.ConnectionString, builder =>
+        {
+            builder.WithCredentialsProvider(saProvider);
+            builder.WithServerCertificates(YcCerts.GetYcServerCertificates());
+        })
+        .Options;
+
+    await using var db = new AppDbContext(options);
+    db.Database.EnsureCreated();
+
+    db.Users.Add(new User { Name = "Alex", Email = "alex@example.com" });
+    db.Users.Add(new User { Name = "Kirill", Email = "kirill@example.com" });
+    db.SaveChanges();
+
+    var users = db.Users.OrderBy(u => u.Id).ToList();
+    Console.WriteLine("Users in database:");
+    foreach (var user in users)
+    {
+        Console.WriteLine($"- {user.Id}: {user.Name} ({user.Email})");
+    }
+});
+
+
+internal class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
+
+internal class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+}
