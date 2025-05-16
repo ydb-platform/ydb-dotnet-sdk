5fe8d6d9f0042aa84884156682638901e5d4c4ee
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-05-16 15:06:32 +0400

    fixes

--- src/EFCore.Ydb/src/Storage/Internal/YdbExecutionStrategy.cs
+++ src/EFCore.Ydb/src/Storage/Internal/YdbExecutionStrategy.cs
@@ -5,14 +5,8 @@ using Ydb.Sdk.Ado;
 namespace EntityFrameworkCore.Ydb.Storage.Internal;
 
 public class YdbExecutionStrategy(ExecutionStrategyDependencies dependencies)
-    : ExecutionStrategy(dependencies, maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(10))
+    : ExecutionStrategy(dependencies, maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(10)) // TODO User settings
 {
     protected override bool ShouldRetryOn(Exception exception)
-        => exception is YdbException ydbException && (
-            TransactionLockInvalidated(ydbException)
-            || false // For other possible exceptions
-        );
-
-    private static bool TransactionLockInvalidated(YdbException exception)
-        => exception.Message.Contains("Transaction locks invalidated");
+        => exception is YdbException { IsTransient: true };
 }
