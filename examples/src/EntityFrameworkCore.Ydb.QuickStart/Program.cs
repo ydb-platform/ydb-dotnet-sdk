6a52218e9f29f0e8a454040d2437bdc1f845f7ac
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-05-26 18:24:47 +0300

    feat: Entity Framework Core YDB Quick Start

--- examples/src/EntityFrameworkCore.Ydb.QuickStart/Program.cs
+++ examples/src/EntityFrameworkCore.Ydb.QuickStart/Program.cs
@@ -3,24 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 await using var db = new BloggingContext();
 
-await db.Database.EnsureDeletedAsync();
-await db.Database.EnsureCreatedAsync();
-
+// Create
 Console.WriteLine("Inserting a new blog");
 db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-
 await db.SaveChangesAsync();
 
+// Read
 Console.WriteLine("Querying for a blog");
 var blog = await db.Blogs
     .OrderBy(b => b.BlogId)
     .FirstAsync();
 
+// Update
 Console.WriteLine("Updating the blog and adding a post");
 blog.Url = "https://devblogs.microsoft.com/dotnet";
-blog.Posts.Add(new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+blog.Posts.Add(
+    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
 await db.SaveChangesAsync();
 
+// Delete
 Console.WriteLine("Delete the blog");
 db.Remove(blog);
 await db.SaveChangesAsync();
