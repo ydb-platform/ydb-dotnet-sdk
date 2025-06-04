68321822d0c1e31dbc9a9b722a4db6cfd3caf9f1
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2025-06-04 19:44:03 +0300

    dev: serverless container sample (#441)

--- examples/EntityFrameworkCore.Ydb.Samples/AddEntity.Sample/Program.cs
+++ examples/EntityFrameworkCore.Ydb.Samples/AddEntity.Sample/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Section_1.HR;
+﻿using Section_1.HR;
 
 InsertDepartments();
 SelectDepartments();
---
+++ examples/Ydb.Sdk.AdoNet.Yandex.Cloud.Serverless.Container/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Ydb.Sdk.Ado;
+using Ydb.Sdk.Yc;
+
+var connectionString = new ConfigurationBuilder()
+                           .SetBasePath(Directory.GetCurrentDirectory())
+                           .AddJsonFile("appsettings.json")
+                           .Build()
+                           .GetConnectionString("ServerlessYDB") ??
+                       throw new InvalidOperationException("ConnectionString.ServerlessYDB is empty.");
+
+var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Information));
+var logger = loggerFactory.CreateLogger<Program>();
+var stopwatch = Stopwatch.StartNew();
+stopwatch.Start();
+
+await using var dataSource = new YdbDataSource(
+    new YdbConnectionStringBuilder(connectionString)
+    {
+        CredentialsProvider = new MetadataProvider(loggerFactory: loggerFactory),
+        LoggerFactory = loggerFactory,
+        DisableDiscovery = true
+    }
+);
+
+await using var ydbCommand = dataSource.CreateCommand();
+ydbCommand.CommandText = "SELECT 'Hello Serverless YDB from Yandex Cloud Serverless Container!'u";
+var scalar = await ydbCommand.ExecuteScalarAsync();
+stopwatch.Stop();
+
+logger.LogInformation("Success request! [Ms: {Ms}], {Select}", stopwatch.ElapsedMilliseconds, scalar);
