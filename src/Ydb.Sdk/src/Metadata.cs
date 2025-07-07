b426020faafab34a0b19385cc54a7c423d21aff8
Ivan Travnikov <39193727+Travonka@users.noreply.github.com>
2025-07-07 18:35:49 +0300

    PID header added (#464)

--- src/Ydb.Sdk/src/Metadata.cs
+++ src/Ydb.Sdk/src/Metadata.cs
@@ -2,13 +2,18 @@
 
 internal static class Metadata
 {
+    // Outgoing headers
     public const string RpcDatabaseHeader = "x-ydb-database";
     public const string RpcAuthHeader = "x-ydb-auth-ticket";
     public const string RpcRequestTypeHeader = "x-ydb-request-type";
     public const string RpcTraceIdHeader = "x-ydb-trace-id";
     public const string RpcSdkInfoHeader = "x-ydb-sdk-build-info";
+    public const string RpcClientPid = "x-ydb-client-pid";
+
+    // Incoming headers
     public const string RpcServerHintsHeader = "x-ydb-server-hints";
     public const string RpcClientCapabilitiesHeader = "x-ydb-client-capabilities";
 
+    //Incoming hints
     public const string GracefulShutdownHint = "session-close";
 }
