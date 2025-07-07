9f296448ab52b9b53a616308ab0644d68a6155b9
bogdangalka <bogdangalka@yandex-team.ru>
2025-07-07 10:25:07 +0300

    Changed field type in SessionPool config

--- src/Ydb.Sdk/src/Pool/SessionPool.cs
+++ src/Ydb.Sdk/src/Pool/SessionPool.cs
@@ -11,8 +11,8 @@ internal abstract class SessionPool<TSession> where TSession : SessionBase<TSess
     private readonly ConcurrentQueue<TSession> _idleSessions = new();
     private readonly int _createSessionTimeoutMs;
     private readonly int _size;
-    private protected readonly SessionPoolConfig Config;
-
+    
+    protected readonly SessionPoolConfig Config;
     protected readonly ILogger<SessionPool<TSession>> Logger;
 
     private volatile int _waitingCount;
