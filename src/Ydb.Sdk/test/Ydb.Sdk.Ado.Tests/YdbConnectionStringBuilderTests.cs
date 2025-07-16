d6f99569d9698bbe703ef902700cb446b962f23d
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2025-07-16 19:35:33 +0300

    dev: MinSessionPool, SessionIdleTimeout, SessionPruningInterval (#476)

--- src/Ydb.Sdk/test/Ydb.Sdk.Ado.Tests/YdbConnectionStringBuilderTests.cs
+++ src/Ydb.Sdk/test/Ydb.Sdk.Ado.Tests/YdbConnectionStringBuilderTests.cs
@@ -12,7 +12,11 @@ public class YdbConnectionStringBuilderTests
         Assert.Equal(2136, ydbConnectionStringBuilder.Port);
         Assert.Equal("localhost", ydbConnectionStringBuilder.Host);
         Assert.Equal("/local", ydbConnectionStringBuilder.Database);
+        Assert.Equal(0, ydbConnectionStringBuilder.MinSessionPool);
         Assert.Equal(100, ydbConnectionStringBuilder.MaxSessionPool);
+        Assert.Equal(5, ydbConnectionStringBuilder.CreateSessionTimeout);
+        Assert.Equal(300, ydbConnectionStringBuilder.SessionIdleTimeout);
+        Assert.Equal(10, ydbConnectionStringBuilder.SessionPruningInterval);
         Assert.Null(ydbConnectionStringBuilder.User);
         Assert.Null(ydbConnectionStringBuilder.Password);
         Assert.Equal(5, ydbConnectionStringBuilder.ConnectTimeout);
@@ -24,7 +28,6 @@ public class YdbConnectionStringBuilderTests
         Assert.Equal(64 * 1024 * 1024, ydbConnectionStringBuilder.MaxReceiveMessageSize);
         Assert.False(ydbConnectionStringBuilder.DisableDiscovery);
         Assert.False(ydbConnectionStringBuilder.DisableServerBalancer);
-        Assert.Equal(5, ydbConnectionStringBuilder.CreateSessionTimeout);
         Assert.False(ydbConnectionStringBuilder.UseTls);
     }
 
@@ -43,17 +46,22 @@ public class YdbConnectionStringBuilderTests
     {
         var connectionString = new YdbConnectionStringBuilder(
             "Host=server;Port=2135;Database=/my/path;User=Kirill;UseTls=true;" +
+            "MinSessionPool=10;MaxSessionPool=50;CreateSessionTimeout=30;" +
+            "SessionIdleTimeout=600;SessionPruningInterval=20;" +
             "ConnectTimeout=30;KeepAlivePingDelay=30;KeepAlivePingTimeout=60;" +
-            "EnableMultipleHttp2Connections=true;CreateSessionTimeout=30;" +
+            "EnableMultipleHttp2Connections=true;" +
             "MaxSendMessageSize=1000000;MaxReceiveMessageSize=1000000;" +
-            "DisableDiscovery=true;DisableServerBalancer=true;" +
-            "MaxSessionPool=50"
+            "DisableDiscovery=true;DisableServerBalancer=true;"
         );
 
         Assert.Equal(2135, connectionString.Port);
         Assert.Equal("server", connectionString.Host);
         Assert.Equal("/my/path", connectionString.Database);
+        Assert.Equal(10, connectionString.MinSessionPool);
         Assert.Equal(50, connectionString.MaxSessionPool);
+        Assert.Equal(30, connectionString.CreateSessionTimeout);
+        Assert.Equal(600, connectionString.SessionIdleTimeout);
+        Assert.Equal(20, connectionString.SessionPruningInterval);
         Assert.Equal("Kirill", connectionString.User);
         Assert.Equal(30, connectionString.ConnectTimeout);
         Assert.Equal(30, connectionString.KeepAlivePingDelay);
@@ -63,15 +71,14 @@ public class YdbConnectionStringBuilderTests
         Assert.Equal(1000000, connectionString.MaxSendMessageSize);
         Assert.Equal(1000000, connectionString.MaxReceiveMessageSize);
         Assert.Equal("Host=server;Port=2135;Database=/my/path;User=Kirill;UseTls=True;" +
+                     "MinSessionPool=10;MaxSessionPool=50;CreateSessionTimeout=30;" +
+                     "SessionIdleTimeout=600;SessionPruningInterval=20;" +
                      "ConnectTimeout=30;KeepAlivePingDelay=30;KeepAlivePingTimeout=60;" +
-                     "EnableMultipleHttp2Connections=True;CreateSessionTimeout=30;" +
+                     "EnableMultipleHttp2Connections=True;" +
                      "MaxSendMessageSize=1000000;MaxReceiveMessageSize=1000000;" +
-                     "DisableDiscovery=True;DisableServerBalancer=True;" +
-                     "MaxSessionPool=50", connectionString.ConnectionString);
+                     "DisableDiscovery=True;DisableServerBalancer=True", connectionString.ConnectionString);
         Assert.True(connectionString.DisableDiscovery);
         Assert.True(connectionString.DisableServerBalancer);
-        Assert.Equal(30, connectionString.CreateSessionTimeout);
-        Assert.Equal(50, connectionString.MaxSessionPool);
     }
 
     [Fact]
