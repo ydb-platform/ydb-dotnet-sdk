3e0111817a20ea01f0697698b1950ce65b0254ff
Ivan Travnikov <travnikov2014@yandex.ru>
2025-08-05 02:15:49 +0300

    wip

---
+++ src/EFCore.Ydb/src/Query/Expressions/Internal/YdbILikeExpression.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EntityFrameworkCore.Ydb.Query.Expressions.Internal;
+
+
+public class YdbILikeExpression : SqlExpression, IEquatable<YdbILikeExpression>
+{
+    private static ConstructorInfo? _quotingConstructor;
+    public virtual SqlExpression Match { get; }
+    public virtual SqlExpression Pattern { get; }
+    public virtual SqlExpression? EscapeChar { get; }
+
+    public YdbILikeExpression(
+        SqlExpression match,
+        SqlExpression pattern,
+        SqlExpression? escapeChar,
+        RelationalTypeMapping? typeMapping)
+        : base(typeof(bool), typeMapping)
+    {
+        Match = match;
+        Pattern = pattern;
+        EscapeChar = escapeChar;
+    }
+
+    protected override Expression VisitChildren(ExpressionVisitor visitor)
+        => Update(
+            (SqlExpression)visitor.Visit(Match),
+            (SqlExpression)visitor.Visit(Pattern),
+            EscapeChar is null ? null : (SqlExpression)visitor.Visit(EscapeChar));
+    
+    public virtual YdbILikeExpression Update(
+        SqlExpression match,
+        SqlExpression pattern,
+        SqlExpression? escapeChar)
+        => match == Match && pattern == Pattern && escapeChar == EscapeChar
+            ? this
+            : new YdbILikeExpression(match, pattern, escapeChar, TypeMapping);
+
+    [Experimental("EF9100")]
+    public override Expression Quote()
+        => New(
+            _quotingConstructor ??= typeof(YdbILikeExpression).GetConstructor(
+                [typeof(SqlExpression), typeof(SqlExpression), typeof(SqlExpression), typeof(RelationalTypeMapping)])!,
+            Match.Quote(),
+            Pattern.Quote(),
+            RelationalExpressionQuotingUtilities.QuoteOrNull(EscapeChar),
+            RelationalExpressionQuotingUtilities.QuoteTypeMapping(TypeMapping));
+
+    public override bool Equals(object? obj)
+        => obj is YdbILikeExpression other && Equals(other);
+
+    public virtual bool Equals(YdbILikeExpression? other)
+        => ReferenceEquals(this, other)
+            || other is not null
+            && base.Equals(other)
+            && Equals(Match, other.Match)
+            && Equals(Pattern, other.Pattern)
+            && Equals(EscapeChar, other.EscapeChar);
+
+    public override int GetHashCode()
+        => HashCode.Combine(base.GetHashCode(), Match, Pattern, EscapeChar);
+
+    protected override void Print(ExpressionPrinter expressionPrinter)
+    {
+        expressionPrinter.Visit(Match);
+        expressionPrinter.Append(" ILIKE ");
+        expressionPrinter.Visit(Pattern);
+
+        if (EscapeChar is not null)
+        {
+            expressionPrinter.Append(" ESCAPE ");
+            expressionPrinter.Visit(EscapeChar);
+        }
+    }
+
+    public override string ToString()
+        => $"{Match} ILIKE {Pattern}{(EscapeChar is null ? "" : $" ESCAPE {EscapeChar}")}";
+}
