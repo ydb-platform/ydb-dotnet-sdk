e2fc15cf875ff161443df7c832b2389fd99699f0
Ivan Travnikov <travnikov2014@yandex.ru>
2025-08-06 15:58:21 +0300

    tests wip

--- src/EFCore.Ydb/test/EntityFrameworkCore.Ydb.FunctionalTests/Query/NorthwindCompiledQueryYdbTest.cs
+++ src/EFCore.Ydb/test/EntityFrameworkCore.Ydb.FunctionalTests/Query/NorthwindCompiledQueryYdbTest.cs
@@ -1,5 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.EntityFrameworkCore.TestModels.Northwind;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace EntityFrameworkCore.Ydb.FunctionalTests.Query;
@@ -44,4 +47,28 @@ public class NorthwindCompiledQueryYdbTest
     public override Task Query_with_array_parameter_async() => Task.CompletedTask;
 
     public override Task Compiled_query_with_max_parameters() => Task.CompletedTask;
+    
+    [Fact]
+    public async Task Array_All_ILike()
+    {
+        await using var context = CreateContext();
+
+        var collection = new[] { "a%", "b%", "c%" };
+        var query = context.Set<Customer>().Where(c => collection.All(y => EF.Functions.ILike(c.Address, y)));
+        var result = await query.ToListAsync();
+
+        Assert.Empty(result);
+
+        AssertSql(
+            """
+            @collection={ 'a%', 'b%', 'c%' } (DbType = Object)
+
+            SELECT c."CustomerID", c."Address", c."City", c."CompanyName", c."ContactName", c."ContactTitle", c."Country", c."Fax", c."Phone", c."PostalCode", c."Region"
+            FROM "Customers" AS c
+            WHERE c."Address" ILIKE ALL (@collection)
+            """);
+    }
+    
+    private void AssertSql(params string[] expected)
+        => Fixture.TestSqlLoggerFactory.AssertBaseline(expected);
 }
