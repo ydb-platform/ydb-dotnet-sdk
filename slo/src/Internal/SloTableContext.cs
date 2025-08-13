3aec00dcd856ed34cc4b0b6e49a00ac32f5bca81
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-08-13 12:09:04 +0300

    delete CPU intensive trotting

--- slo/src/Internal/SloTableContext.cs
+++ slo/src/Internal/SloTableContext.cs
@@ -210,7 +210,7 @@ public abstract class SloTableContext<T> : ISloContext
 
                         if (!lease.IsAcquired)
                         {
-                            continue;
+                            await Task.Delay(IntervalMs, cancellationTokenSource.Token);
                         }
 
                         try
