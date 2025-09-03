71bc06c917bb472b14ac3e2a87c3ba14ae3712de
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-09-03 17:23:30 +0700

    fix test

--- src/Ydb.Sdk/test/Ydb.Sdk.Ado.Tests/YdbDataSourceTests.cs
+++ src/Ydb.Sdk/test/Ydb.Sdk.Ado.Tests/YdbDataSourceTests.cs
@@ -233,7 +233,7 @@ public class YdbDataSourceTests : TestBase
                     CommandText = $"UPDATE {tableName} SET count = @count + 1 WHERE id = 1",
                     Parameters = { new YdbParameter { Value = count, ParameterName = "count" } }
                 }.ExecuteNonQueryAsync();
-            }));
+            }, new YdbRetryPolicyConfig { FastBackoffBaseMs = 20 }));
         }
 
         await Task.WhenAll(tasks);
