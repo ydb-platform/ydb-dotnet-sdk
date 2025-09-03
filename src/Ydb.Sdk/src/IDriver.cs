315702f99cb10343c8247e031b0e6f04f9f71d07
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-09-04 01:55:31 +0700

    fix race

--- src/Ydb.Sdk/src/IDriver.cs
+++ src/Ydb.Sdk/src/IDriver.cs
@@ -69,7 +69,7 @@ public abstract class BaseDriver : IDriver
 
     private int _ownerCount;
 
-    protected int Disposed;
+    protected volatile int Disposed;
 
     internal BaseDriver(
         DriverConfig config,
@@ -211,7 +211,7 @@ public abstract class BaseDriver : IDriver
 
     public ILoggerFactory LoggerFactory { get; }
     public void RegisterOwner() => _ownerCount++;
-    public bool IsDisposed => Volatile.Read(ref Disposed) == 1;
+    public bool IsDisposed => Disposed == 1;
 
     public void Dispose() => DisposeAsync().AsTask().GetAwaiter().GetResult();
 
