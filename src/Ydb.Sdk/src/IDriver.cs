91bbb1a625b1c7c6532bd39931ea613f6505a26a
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-09-05 16:44:43 +0700

    fix

--- src/Ydb.Sdk/src/IDriver.cs
+++ src/Ydb.Sdk/src/IDriver.cs
@@ -67,7 +67,7 @@ public abstract class BaseDriver : IDriver
     internal readonly GrpcChannelFactory GrpcChannelFactory;
     internal readonly ChannelPool<GrpcChannel> ChannelPool;
 
-    private volatile int _ownerCount;
+    private int _ownerCount;
 
     protected volatile int Disposed;
 
@@ -210,14 +210,14 @@ public abstract class BaseDriver : IDriver
     }
 
     public ILoggerFactory LoggerFactory { get; }
-    public void RegisterOwner() => ++_ownerCount;
+    public void RegisterOwner() => Interlocked.Increment(ref _ownerCount);
     public bool IsDisposed => Disposed == 1;
 
     public void Dispose() => DisposeAsync().AsTask().GetAwaiter().GetResult();
 
     public async ValueTask DisposeAsync()
     {
-        if (--_ownerCount <= 0 && Interlocked.CompareExchange(ref Disposed, 1, 0) == 0)
+        if (Interlocked.Decrement(ref _ownerCount) <= 0 && Interlocked.CompareExchange(ref Disposed, 1, 0) == 0)
         {
             await ChannelPool.DisposeAsync();
 
