7f0473cec8825061b405c865892a06a57310b7b3
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2025-12-10 10:07:26 +0300

    Added support `DELETE` batching for EF Core SaveChanges operations (#576)

--- src/EFCore.Ydb/test/EntityFrameworkCore.Ydb.FunctionalTests/YdbModificationCommandBatchTest.cs
+++ src/EFCore.Ydb/test/EntityFrameworkCore.Ydb.FunctionalTests/YdbModificationCommandBatchTest.cs
@@ -10,6 +10,12 @@ namespace EntityFrameworkCore.Ydb.FunctionalTests;
 public class YdbModificationCommandBatchTest
 {
     private readonly TestSqlLoggerFactory _assertSql = new();
+    private readonly Order _order1 = new() { CreatedAt = DateTime.Now };
+    private readonly Order _order2 = new() { CreatedAt = DateTime.Now };
+    private readonly Order _order3 = new() { Id = -1, CreatedAt = DateTime.Now, Discount = 20 };
+    private readonly Order _order4 = new() { Id = -2, CreatedAt = DateTime.Now, Discount = 30 };
+    private readonly Order _order5 = new() { CreatedAt = DateTime.Now, Discount = 20 };
+    private readonly Order _order6 = new() { CreatedAt = DateTime.Now, Discount = 30 };
 
     [Fact]
     public async Task InsertBatchTest()
@@ -17,21 +23,9 @@ public class YdbModificationCommandBatchTest
         await using var testStore = YdbTestStoreFactory.Instance.Create("YdbModificationCommandBatchTest");
         await using var dbContext = new TestEntityDbContext(_assertSql);
         await testStore.CleanAsync(dbContext);
-
         await dbContext.Database.EnsureCreatedAsync();
-        var order1 = new Order { CreatedAt = DateTime.Now };
-        var order2 = new Order { CreatedAt = DateTime.Now };
-        var order3 = new Order { Id = -1, CreatedAt = DateTime.Now, Discount = 20 };
-        var order4 = new Order { Id = -2, CreatedAt = DateTime.Now, Discount = 30 };
-        var order5 = new Order { CreatedAt = DateTime.Now, Discount = 20 };
-        var order6 = new Order { CreatedAt = DateTime.Now, Discount = 30 };
 
-        dbContext.Orders.Add(order1);
-        dbContext.Orders.Add(order2);
-        dbContext.Orders.Add(order3);
-        dbContext.Orders.Add(order4);
-        dbContext.Orders.Add(order5);
-        dbContext.Orders.Add(order6);
+        dbContext.Orders.AddRange(_order1, _order2, _order3, _order4, _order5, _order6);
         await dbContext.SaveChangesAsync();
         AssertSql(
             """
@@ -46,19 +40,42 @@ public class YdbModificationCommandBatchTest
             INSERT INTO `Order` SELECT * FROM AS_TABLE($batch_value_2);
             """);
 
-        Assert.Equal(1, order1.Id);
-        Assert.Equal(2, order2.Id);
-        Assert.Equal(-1, order3.Id);
-        Assert.Equal(-2, order4.Id);
-        Assert.Equal(3, order5.Id);
-        Assert.Equal(4, order6.Id);
+        Assert.Equal(1, _order1.Id);
+        Assert.Equal(2, _order2.Id);
+        Assert.Equal(-1, _order3.Id);
+        Assert.Equal(-2, _order4.Id);
+        Assert.Equal(3, _order5.Id);
+        Assert.Equal(4, _order6.Id);
 
-        Assert.Equal(0, order1.Discount);
-        Assert.Equal(0, order2.Discount);
-        Assert.Equal(20, order3.Discount);
-        Assert.Equal(30, order4.Discount);
-        Assert.Equal(20, order5.Discount);
-        Assert.Equal(30, order6.Discount);
+        Assert.Equal(0, _order1.Discount);
+        Assert.Equal(0, _order2.Discount);
+        Assert.Equal(20, _order3.Discount);
+        Assert.Equal(30, _order4.Discount);
+        Assert.Equal(20, _order5.Discount);
+        Assert.Equal(30, _order6.Discount);
+    }
+
+    [Fact]
+    public async Task DeleteBatchTest()
+    {
+        await using var testStore = YdbTestStoreFactory.Instance.Create("YdbModificationCommandBatchTest");
+        await using var dbContext = new TestEntityDbContext(_assertSql);
+        await testStore.CleanAsync(dbContext);
+        await dbContext.Database.EnsureCreatedAsync();
+
+        dbContext.Orders.AddRange(_order1, _order2, _order3, _order4, _order5, _order6);
+        await dbContext.SaveChangesAsync();
+        dbContext.Orders.RemoveRange(_order1, _order2, _order3, _order4, _order5, _order6);
+        await dbContext.SaveChangesAsync();
+
+        AssertSql(
+            """
+            $batch_value_0='?' (DbType = Object)
+
+            DELETE FROM `Order` ON SELECT * FROM AS_TABLE($batch_value_0);
+            """);
+
+        Assert.Empty(dbContext.Orders);
     }
 
     private void AssertSql(params string[] expected) => _assertSql.AssertBaseline(expected, false);
