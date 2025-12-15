d18388378812206874f924916e037e7cb2705ef1
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2025-12-15 19:38:11 +0300

    kek

--- src/EFCore.Ydb/test/EntityFrameworkCore.Ydb.FunctionalTests/Migrations/YdbMigrationsInfrastructureTest.cs
+++ src/EFCore.Ydb/test/EntityFrameworkCore.Ydb.FunctionalTests/Migrations/YdbMigrationsInfrastructureTest.cs
@@ -57,7 +57,6 @@ public class YdbMigrationsInfrastructureTest(YdbMigrationsInfrastructureTest.Ydb
     public override Task Can_apply_second_migration_in_parallel_async() =>
         base.Can_apply_second_migration_in_parallel_async();
 
-#if !EFCORE9
     [ConditionalFact(Skip = "TODO")]
     public override void Can_apply_two_migrations_in_transaction() =>
         base.Can_apply_two_migrations_in_transaction();
@@ -65,7 +64,6 @@ public class YdbMigrationsInfrastructureTest(YdbMigrationsInfrastructureTest.Ydb
     [ConditionalFact(Skip = "TODO")]
     public override Task Can_apply_two_migrations_in_transaction_async() =>
         base.Can_apply_two_migrations_in_transaction_async();
-#endif
 
     [ConditionalFact(Skip = "TODO")]
     public override Task Can_generate_idempotent_up_and_down_scripts() =>
