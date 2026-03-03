15c6a75c4f52bc83564d3b4d53699d9445c3240d
Lil_sf <stolbovdimon@gmail.com>
2026-03-03 04:09:26 +0300

    session version beta

--- src/Ydb.Sdk/src/Coordinator/Impl/CoordinationSession.cs
+++ src/Ydb.Sdk/src/Coordinator/Impl/CoordinationSession.cs
@@ -1,7 +1,10 @@
-﻿using Google.Protobuf;
+﻿using System.Collections.Concurrent;
+using Google.Protobuf;
 using Ydb.Coordination;
 using Ydb.Coordination.V1;
 using Ydb.Sdk.Ado;
+using Ydb.Sdk.Ado.RetryPolicy;
+using Ydb.Sdk.Coordinator.Description;
 using Ydb.Sdk.Coordinator.Settings;
 
 namespace Ydb.Sdk.Coordinator.Impl;
@@ -10,21 +13,43 @@ public class CoordinationSession
 {
     private readonly IDriver _driver;
     private readonly CoordinationNodeSettings _settings;
+    private readonly YdbRetryPolicyExecutor _ydbRetryPolicyExecutor;
+
+
+    private ulong _sessionId = 0;
+    private ulong _reqIdCounter = 0;
+    private ulong _seqNo = 0;
+
+
+    // Tasks for waiting SessionStarted and SessionStopped
+    private TaskCompletionSource? _sessionStartedTcs;
+    private TaskCompletionSource? _sessionStoppedTcs;
+
+    // Bidirectional stream handler
     private IBidirectionalStream<SessionRequest, SessionResponse>? _stream;
+    private readonly ConcurrentDictionary<ulong, PendingRequest<bool, SessionRequest>> _pendingRequests = new();
+    private readonly List<SessionRequest> _fireAndForgetRequests = new();
 
-    //private readonly string _path;
-    //private readonly ulong _timeoutMillis;
-    //private readonly string _description;
-    //private ulong _sessionId = 0;
-    //private ulong _reqIdCounter = 0;
-    //private ulong _seqNo = 0;
+    // Reconnection state
+    private bool _closed = false;
 
-    // Resolve functions for waiting SessionStarted and SessionStopped
-    //private Func<Task> _sessionStartedResolve = null;
-    //private Func<Task> _sessionStoppedResolve = null;
+    // Map of _reqIdCounter to semaphore name for watch tracking
+    private readonly ConcurrentDictionary<ulong, string> _watchedSemaphores =
+        new();
 
-    public CoordinationSession(IDriver driver, CoordinationNodeSettings settings)
+
+    private readonly TaskCompletionSource _firstSessionStartedTcs =
+        new(TaskCreationOptions.RunContinuationsAsynchronously);
+
+    // EVENTS
+    //public event Action<SessionExpiredEvent>? SessionExpired;
+    //public event Action<SemaphoreChangedEvent>? SemaphoreChanged;    
+
+    private readonly CancellationTokenSource _disposeCts = new();
+
+    public CoordinationSession(IDriver driver, CoordinationNodeSettings settings, IRetryPolicy retryPolicy)
     {
+        _ydbRetryPolicyExecutor = new YdbRetryPolicyExecutor(retryPolicy);
         _driver = driver;
         _settings = settings;
         Initialize();
@@ -35,6 +60,7 @@ public class CoordinationSession
     {
         try
         {
+            // _ydbRetryPolicyExecutor.ExecuteAsync();
             await StreamCall();
         }
         catch (Exception)
@@ -48,11 +74,166 @@ public class CoordinationSession
     // простой пример создания stream, отправляем серверу сообщение о старте и получаем ответ от сервера
     private async Task StreamCall()
     {
+        _stream = await _driver.BidirectionalStreamCall(CoordinationService.SessionMethod, _settings);
+        await RunProcessingStreamResponse();
+        await SendStartSession();
+    }
+
+
+    private async Task RunProcessingStreamResponse()
+    {
+        try
+        {
+            while (await _stream!.MoveNextAsync())
+            {
+                var response = _stream.Current;
+                switch (response.ResponseCase)
+                {
+                    case SessionResponse.ResponseOneofCase.Ping:
+                        // Server sent ping, respond with pong
+                        await HandlePing(response);
+                        break;
+                    case SessionResponse.ResponseOneofCase.Failure:
+                        await HandleFailure(response);
+                        break;
+                    case SessionResponse.ResponseOneofCase.SessionStarted:
+                        HandleSessionStarted(response);
+                        break;
+                    case SessionResponse.ResponseOneofCase.SessionStopped:
+                        HandleSessionStopped(response);
+                        break;
+                    case SessionResponse.ResponseOneofCase.AcquireSemaphorePending:
+                        break;
+                    case SessionResponse.ResponseOneofCase.DescribeSemaphoreChanged:
+                        break;
+                    case SessionResponse.ResponseOneofCase.AcquireSemaphoreResult:
+                        break;
+                }
+                /*
+                 * None,
+    Ping,
+    Pong,
+    Failure,
+    SessionStarted,
+    SessionStopped,
+    Unsupported6,
+    Unsupported7,
+    AcquireSemaphorePending,
+    AcquireSemaphoreResult,
+    ReleaseSemaphoreResult,
+    DescribeSemaphoreResult,
+    DescribeSemaphoreChanged,
+    CreateSemaphoreResult,
+    UpdateSemaphoreResult,
+    DeleteSemaphoreResult,
+    Unsupported16,
+    Unsupported17,
+    Unsupported18,
+                 */
+
+                //var reqId = _extractReqId(response); //_extractReqId(response);
+                ulong? reqId = 1;
+                if (reqId.HasValue &&
+                    _pendingRequests.TryRemove(reqId.Value, out var pending))
+                {
+                    try
+                    {
+                        //var result = _extractResult(response);
+                        //pending.Tcs.TrySetResult(result);
+                    }
+                    catch (Exception ex)
+                    {
+                        pending.Tcs.TrySetException(ex);
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            // Logger.LogError(e, "ReaderSession[{SessionId}] have error on processing server messages", SessionId);
+        }
+        finally
+        {
+            // NOTE
+        }
+    }
+
+    private async Task HandlePing(SessionResponse response)
+    {
+        var opaque = response.Ping.Opaque;
+        var pongRequest = new SessionRequest()
+        {
+            Pong = new SessionRequest.Types.PingPong()
+            {
+                Opaque = opaque
+            }
+        };
+        await _stream!.Write(pongRequest);
+    }
+
+    private async Task HandleFailure(SessionResponse response)
+    {
+        var failure = response.Failure;
+        if (failure.Status == StatusIds.Types.StatusCode.BadSession |
+            failure.Status == StatusIds.Types.StatusCode.SessionExpired)
+        {
+            // If session is expired or not accessible, reset session ID to create a new session on reconnect
+            //var expiredId = _sessionId;
+            _sessionId = 0;
+            _watchedSemaphores.Clear();
+
+            // // Emit sessionExpired event to notify user
+        }
+
+        await _stream!.RequestStreamComplete();
+    }
+
+    private void HandleSessionStarted(SessionResponse response)
+    {
+        _sessionId = response.SessionStarted.SessionId;
+        // Resolve the sessionStarted promise
+        _sessionStartedTcs?.TrySetResult();
+        _sessionStartedTcs = null;
+
+        // Resolve the first session started promise (only once)
+        _firstSessionStartedTcs.TrySetResult();
+    }
+
+    private void HandleSessionStopped(SessionResponse response)
+    {
+        //_sessionId = response.SessionStopped.SessionId; // в логи записываем
+        _sessionStoppedTcs?.TrySetResult();
+        _sessionStoppedTcs = null;
+    }
+
+    /*
+    private void HandleSemaphoreChanged(SessionResponse.Types.DescribeSemaphoreChanged change)
+    {
+        if (_watchedSemaphores.TryRemove(change.ReqId, out var name))
+        {
+
+            SemaphoreChanged?.Invoke(new SemaphoreChangedEvent
+            {
+                Name = name,
+                DataChanged = change.DataChanged,
+                OwnersChanged = change.OwnersChanged
+            });
+        }
+    }
+    */
+
+    private async Task SendStartSession()
+    {
+        if (_stream == null)
+            return;
+
+        _sessionStartedTcs =
+            new(TaskCreationOptions.RunContinuationsAsynchronously);
+
         ulong reqId = 1;
         var node = "";
         // var timeout = new TimeSpan(5);
         var key = ByteString.Empty;
-        _stream = await _driver.BidirectionalStreamCall(CoordinationService.SessionMethod, _settings);
         var initRequestStart = new SessionRequest
         {
             SessionStart =
@@ -65,58 +246,8 @@ public class CoordinationSession
         };
 
         await _stream.Write(initRequestStart);
-
-        if (!await _stream.MoveNextAsync())
-        {
-            // что-то делаем,если сообщения не отправилось
-        }
-        //прием сообщения
-        // var receivedInitMessage = _stream.Current;
+        await _sessionStartedTcs.Task;
     }
-
-    /*
-    private async void HandleResponse(SessionResponse response)
-    {
-        switch (response.ResponseCase)
-        {
-            case SessionResponse.ResponseOneofCase.Ping:
-                // Server sent ping, respond with pong
-                var opaque = response.Ping.Opaque;
-                var pongRequest = new SessionRequest()
-                {
-                    Pong = new SessionRequest.Types.PingPong()
-                    {
-                        Opaque = opaque
-                    }
-                };
-                await _stream.Write(pongRequest);
-                break;
-
-            case SessionResponse.ResponseOneofCase.Failure:
-                var failure = response.Failure;
-                if (failure.Status == StatusIds.Types.StatusCode.BadSession |
-                    failure.Status == StatusIds.Types.StatusCode.SessionExpired)
-                {
-                    // If session is expired or not accessible, reset session ID to create a new session on reconnect
-                }
-
-                //await stream.RequestStreamComplete();
-                break;
-            case SessionResponse.ResponseOneofCase.SessionStarted:
-                _sessionId = response.SessionStarted.SessionId;
-                break;
-            case SessionResponse.ResponseOneofCase.SessionStopped:
-                _sessionId = response.SessionStopped.SessionId;
-                break;
-            case SessionResponse.ResponseOneofCase.AcquireSemaphorePending:
-                break;
-            case SessionResponse.ResponseOneofCase.DescribeSemaphoreChanged:
-                break;
-            default:
-                break;
-        }
-    }
-    */
 
     public async Task CreateSemaphore(string name, ulong limit, byte[]? data)
     {
---
+++ src/Ydb.Sdk/src/Coordinator/Impl/PendingRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Ydb.Sdk.Coordinator.Impl;
+
+internal sealed class PendingRequest<TResult, TRequest>
+{
+    public TaskCompletionSource<TResult?> Tcs { get; }
+    public TRequest Request { get; }
+
+    public PendingRequest(TaskCompletionSource<TResult?> tcs, TRequest request)
+    {
+        Tcs = tcs;
+        Request = request;
+    }
+}
