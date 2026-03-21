0a9b62694120470a4fed2df33a53dbbbe4114dc2
Lil_sf <stolbovdimon@gmail.com>
2026-03-21 14:20:21 +0300

    add test appha 1

--- src/Ydb.Sdk/test/Ydb.Sdk.Coordination.Tests/CoordinationClientIntegrationTests.cs
+++ src/Ydb.Sdk/test/Ydb.Sdk.Coordination.Tests/CoordinationClientIntegrationTests.cs
@@ -1,9 +1,10 @@
 ﻿using Xunit;
+using Ydb.Coordination;
 using Ydb.Sdk.Ado;
 using Ydb.Sdk.Coordination.Description;
 using Ydb.Sdk.Coordination.Settings;
-using Ydb.Sdk.Topic;
-using Ydb.Sdk.Topic.Writer;
+using ConsistencyMode = Ydb.Sdk.Coordination.Description.ConsistencyMode;
+using RateLimiterCountersMode = Ydb.Sdk.Coordination.Description.RateLimiterCountersMode;
 
 namespace Ydb.Sdk.Coordination.Tests;
 
@@ -23,40 +24,68 @@ public class CoordinationClientIntegrationTests
                 .WithRateLimiterCountersMode(RateLimiterCountersMode.Detailed)
         };
 
+        var describeCoordinationNodeSettings = new DescribeCoordinationNodeSettings();
+
+        var oldNodeConfig = coordinationNodeSettings.Config.ToProto();
         var pathNode = "/local/test";
-        try
-        {
-            await _coordinationClient.CreateNode(pathNode, coordinationNodeSettings);
-        }
-        catch (YdbException e)
-        {
-            Console.WriteLine(e.Message);
-        }
+
+        await _coordinationClient.CreateNode(pathNode, coordinationNodeSettings);
+        var describeNode = await _coordinationClient.DescribeNode(pathNode, describeCoordinationNodeSettings);
+        var describeNodeConfig = describeNode.ToProto();
+
+        Assert.Equal(oldNodeConfig.SelfCheckPeriodMillis, describeNodeConfig.SelfCheckPeriodMillis);
+        Assert.Equal(oldNodeConfig.SessionGracePeriodMillis, describeNodeConfig.SessionGracePeriodMillis);
+        Assert.Equal(oldNodeConfig.ReadConsistencyMode, describeNodeConfig.ReadConsistencyMode);
+        Assert.Equal(oldNodeConfig.AttachConsistencyMode, describeNodeConfig.AttachConsistencyMode);
+        Assert.Equal(oldNodeConfig.RateLimiterCountersMode, describeNodeConfig.RateLimiterCountersMode);
+
+       
+       // await _coordinationClient.DropNode()
     }
-    
+
+
     [Fact]
     public async Task AlterNode()
     {
         var coordinationNodeSettings = new CoordinationNodeSettings
         {
             Config = NodeConfig.Create()
-                .WithDurationsConfig(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(3))
+                .WithDurationsConfig(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(25))
                 .WithReadConsistencyMode(ConsistencyMode.Relaxed)
                 .WithAttachConsistencyMode(ConsistencyMode.Relaxed)
                 .WithRateLimiterCountersMode(RateLimiterCountersMode.Detailed)
         };
 
+        var alterCoordinationNodeSettings = new CoordinationNodeSettings
+        {
+            Config = NodeConfig.Create()
+                .WithDurationsConfig(TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(20))
+                .WithReadConsistencyMode(ConsistencyMode.Relaxed)
+                .WithAttachConsistencyMode(ConsistencyMode.Relaxed)
+                .WithRateLimiterCountersMode(RateLimiterCountersMode.Aggregated)
+        };
+
+        var describeCoordinationNodeSettings = new DescribeCoordinationNodeSettings();
+
+        var alterNodeConfig = alterCoordinationNodeSettings.Config.ToProto();
         var pathNode = "/local/test";
-        try
-        {
-            await _coordinationClient.CreateNode(pathNode, coordinationNodeSettings);
-        }
-        catch (YdbException e)
-        {
-            Console.WriteLine(e.Message);
-        }
+
+        await _coordinationClient.CreateNode(pathNode, coordinationNodeSettings);
+        await _coordinationClient.AlterNode(pathNode, alterCoordinationNodeSettings);
+
+        var describeNode = await _coordinationClient.DescribeNode(pathNode, describeCoordinationNodeSettings);
+        var describeNodeConfig = describeNode.ToProto();
+
+        Assert.Equal(alterNodeConfig.SelfCheckPeriodMillis, describeNodeConfig.SelfCheckPeriodMillis);
+        Assert.Equal(alterNodeConfig.SessionGracePeriodMillis, describeNodeConfig.SessionGracePeriodMillis);
+        Assert.Equal(alterNodeConfig.ReadConsistencyMode, describeNodeConfig.ReadConsistencyMode);
+        Assert.Equal(alterNodeConfig.AttachConsistencyMode, describeNodeConfig.AttachConsistencyMode);
+        Assert.Equal(alterNodeConfig.RateLimiterCountersMode, describeNodeConfig.RateLimiterCountersMode);
+
+        await _coordinationClient.CreateNode(pathNode, coordinationNodeSettings);
     }
-    
+
+    /*
     [Fact]
     public async Task DescribeNode()
     {
@@ -79,4 +108,5 @@ public class CoordinationClientIntegrationTests
             Console.WriteLine(e.Message);
         }
     }
+    */
 }
